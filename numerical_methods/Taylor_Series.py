32ef7dbf492ad3ae4ad83b1a7a3c28c373433db5
keith-moreno <108557464+k1tooy@users.noreply.github.com>
2024-02-20 12:46:29 +0800

    feat: add formula for taylor series

--- numerical_methods/Taylor_Series.py
+++ numerical_methods/Taylor_Series.py
@@ -4,12 +4,18 @@ import numpy as np
 import math
 import matplotlib.pyplot as plt
 
-def get_taylor_approx(expr, x: float, n: int= 1):
+def get_taylor_approx(expr, x: float=1, a:float=0, n: int= 1):
     var = smp.symbols("x")
-    dx_expr = smp.diff(expr,var,n)
-    result = dx_expr.evalf(subs={var: x})
+    result = 0
+    for i in range(n):
+        dx_expr = smp.diff(expr,var,i)
+        dx_val = dx_expr.evalf(subs={var: a},chop=True)
+        numerator = (x-a)**i
+        denominator = math.factorial(i)
+        result += (numerator*dx_val)/denominator
+    result = str(result).rstrip('0').rstrip('.') if '.' in str(result) else str(result) #delete trailing zeros
     return result
 
 if __name__ == "__main__":
     x, y = smp.symbols("x y")
-    print(get_taylor_approx(x**3,1,2))
+    print(get_taylor_approx(x**3,x=5,n=10))
