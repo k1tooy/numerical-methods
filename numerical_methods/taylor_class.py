3680b9bda0ee29e7e1510a6aa01b7ca77481a80b
keith-moreno <108557464+k1tooy@users.noreply.github.com>
2024-02-21 21:32:59 +0800

    fix: increased the value to 100 for n terms of the series for better approximation

--- numerical_methods/taylor_class.py
+++ numerical_methods/taylor_class.py
@@ -3,7 +3,7 @@ import numpy as np
 import math
 
 class Taylor_Series:
-    def __init__(self,expr,x:float = 0,a:float=0,n:int=10) -> None:
+    def __init__(self,expr,x:float = 0,a:float=0,n:int=100) -> None:
         self.expr = expr
         self.x = x
         self.a = a
@@ -18,15 +18,17 @@ class Taylor_Series:
             numerator = (self.x - self.a)**i
             denominator = math.factorial(i)
             result += (numerator*dx_val)/denominator
-            print(f"{dx_expr}")
         result = str(result).rstrip('0').rstrip('.') if '.' in str(result) else str(result) #delete trailing zeros
         return result
 
     def expression(self):
         return self.expr
+    
+    def graph_series(self):
+        pass
 
 if __name__ == "__main__":
     x, y = smp.symbols("x y")
-    example = Taylor_Series(smp.cos(x),x=1)
+    example = Taylor_Series(smp.cos(x),x=6,n=100)
     approx = example.get_approx()
-    print(approx)
+    print(f"Approximation: {approx}")
