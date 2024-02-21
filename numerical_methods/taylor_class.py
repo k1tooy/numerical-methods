c837ece2d7cbd0d2b9aa4372b098da09dc0eeb34
keith-moreno <108557464+k1tooy@users.noreply.github.com>
2024-02-21 21:00:05 +0800

    feat: Taylor Series Class

---
+++ numerical_methods/taylor_class.py
@@ -0,0 +1,21 @@
+    def get_approx(self):
+        var = smp.symbols("x")
+        result = 0
+        for i in range(self.n):
+            dx_expr = smp.diff(self.expr,var,i)
+            dx_val = dx_expr.evalf(subs={var: self.a})
+            numerator = (self.x - self.a)**i
+            denominator = math.factorial(i)
+            result += (numerator*dx_val)/denominator
+            print(f"{dx_expr}")
+        result = str(result).rstrip('0').rstrip('.') if '.' in str(result) else str(result) #delete trailing zeros
+        return result
+
+    def expression(self):
+        return self.expr
+
+if __name__ == "__main__":
+    x, y = smp.symbols("x y")
+    example = Taylor_Series(smp.cos(x),x=1)
+    approx = example.get_approx()
+    print(approx)
