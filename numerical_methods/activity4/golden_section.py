adfff24d83dd3da121271aeb25aa2240786deb51
Keith Moreno <108557464+k1tooy@users.noreply.github.com>
2024-04-09 00:40:58 +0800

    fix: add and remove comments in golden section

--- numerical_methods/activity4/golden_section.py
+++ numerical_methods/activity4/golden_section.py
@@ -1,12 +1,19 @@
 def golden_section(func, a, b, tol) -> dict:
+    # get them bounds right
     if a >= b:
         raise ValueError("a must be smaller than b")
 
     phi = (1+ 5**0.5)/2
     d = (phi - 1)*(b - a)
+
+    # find new values
     x1 = a + d
     x2 = b - d
+
+    # for comparison with tolerance
     c = abs(x1 - x2)
+
+    # approximation of the maximum
     avg = (x1 + x2)/2
 
     # base case
@@ -21,7 +28,7 @@ def golden_section(func, a, b, tol) -> dict:
             return golden_section(func ,x1, b, tol)
 
 if __name__ == "__main__":
-    # make f(x) be -f(x) to find maximum
+    # example function 
     f = lambda x: x**2 - 6*x + 15
 
     print(golden_section(f, 1, 4, 0.01))
