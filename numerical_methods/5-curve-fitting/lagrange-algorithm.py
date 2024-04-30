0861c931e473e7cc0eb9b18fe228ed345c8e6ac0
Keith Moreno <108557464+k1tooy@users.noreply.github.com>
2024-04-30 22:17:34 +0800

    add: docstring and comments

--- numerical_methods/5-curve-fitting/lagrange-algorithm.py
+++ numerical_methods/5-curve-fitting/lagrange-algorithm.py
@@ -1,9 +1,28 @@
-def my_lagrange(x, y, X):
+# Tigamni la na it gamit hit curve fitting kay pagbiling hin function na maangay hit im set of points.
+# Bagat reverse engineering hit function bagat tibalik tim step
+# Di ba han algebra may function ka magbiling kan points pero didi kay baliktad given kan points tagi ak function sugad
+
+def my_lagrange(x, y, X) -> float:
+    """
+    Curve fitting using lagrange algorithm
+
+    arguments:
+        x -- x-values of points
+        y -- y-values of points
+        X -- x-value of the intermediate point to calculate
+
+    returns:
+        Y:float -- the estimated y-value
+    """
+
     Y = 0
+    # amo na inen dinhi an formula gud
     for n in range(len(x)):
         product = y[n]
         for i in range(len(x)):
-            if x[i] != x[n]:
+            # inen dinhi amo inen an bagat dako na pi ba
+            if x[i] != x[n]:  # handle ZeroDivisionError
+                # make a polynomial function that is zero except at x-value in concern
                 numerator = X - x[i]
                 denominator = x[n] - x[i]
                 product *= (numerator/denominator)
