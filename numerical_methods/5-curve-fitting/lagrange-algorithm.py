958e278209165e282cbf5a4531bc484c4a79f4f4
Keith Moreno <108557464+k1tooy@users.noreply.github.com>
2024-04-30 21:48:52 +0800

    add: lagrange algorithm implementation

---
+++ numerical_methods/5-curve-fitting/lagrange-algorithm.py
@@ -0,0 +1,18 @@
+def my_lagrange(x, y, X):
+    Y = 0
+    for n in range(len(x)):
+        product = y[n]
+        for i in range(len(x)):
+            if x[i] != x[n]:
+                numerator = X - x[i]
+                denominator = x[n] - x[i]
+                product *= (numerator/denominator)
+        Y += product
+    return Y
+
+
+if __name__ == "__main__":
+    x = [1, 2, 3, 4, 5]
+    y = [2, 4, 6, 8, 10]
+    Y = my_lagrange(x, y, 3)
+    print(Y)
